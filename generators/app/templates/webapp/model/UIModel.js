41da1fab2c5f2cee45eb28b03086ffcda13150c4
Joseph BERTHE <joseph.berthe@tutanota.com>
2020-02-06 13:38:35 +0100

    Add UIModel template

---
+++ generators/app/templates/webapp/model/UIModel.js
@@ -0,0 +1,29 @@
+sap.ui.define(["sap/ui/base/Object", "sap/ui/model/json/JSONModel"], function (BaseObject, JSONModel) { 
+
+ BaseObject.extend("<%= fullNamespace  %>.UIModel", {
+      oComponentCtrl: null,
+      oModel: null,
+      
+      constructor: function(oComponentCtrl) {
+        this.oComponentCtrl = oComponentCtrl;
+      },
+      
+      generate: function(){
+        this.oModel = new JSONModel({
+             mySample: '',
+             myTable: []
+        });
+        
+        return this.oModel;
+      },
+      
+      getMySample: function(){
+          return this.oModel.getProperty("/mySample");
+      },
+      
+      setMySample: function(sNewValue){
+         this.oModel.setProperty("/mySample", sNewValue);
+      }
+
+}
+ 
