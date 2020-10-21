c5d5b86c9f5522607081ea95d371fd3af6718065
Joseph BERTHE <joseph.berthe@saphirnet.ch>
2020-10-21 21:43:09 +0200

    Ajout sub generator in test

---
+++ test/01_createApp.js
@@ -0,0 +1,60 @@
+/**
+ * Copyright 2019 Joseph BERTHE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const helpers = require('yeoman-test');
+const path = require('path');
+const assert = require('yeoman-assert');
+const rimraf = require('rimraf');
+const { doesNotMatch } = require('yeoman-assert');
+var SANDBOX = path.resolve(__dirname, 'sandbox');
+
+var temp = require('temp').track();
+
+describe('Create project infolder', function () {
+	before(function (done) {
+		helpers.testDirectory(SANDBOX, done);
+	});
+
+	after(function (done) {
+		//	temp.cleanup();
+		done();
+		//	rimraf(path.join(__dirname, 'temp'), done);
+	});
+
+	describe("Generate the root application", () => {
+		it("Load generator", () => {
+			return  helpers.run(path.join(__dirname, '../generators/app'))
+				.cd(SANDBOX)
+				//.inDir(path.join(__dirname, 'tmp'))
+				//.inTmpDir()
+				.withPrompts({
+					projectname: "ProjectCDNNoService_UT",
+					name_space: "ch.my.company.module",
+					is_local_library: "CDN",
+					UI5Version: "1.71.26",
+					isODataConf: "No"
+				});
+
+		});
+
+		it("Check version", () => {
+
+				assert.fileContent('webapp/test/flpSandboxCDN.html', "1.71.26");
+
+		});
+	});
+
+
+});
